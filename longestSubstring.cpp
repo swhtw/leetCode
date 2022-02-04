9220c4b27a65becd32c0970c9a6edf1a983e968f
Shengwei Huang <huang6275@gmail.com>
2022-02-04 14:54:00 +0800

    add Longest Substring Without Repeating Characters

---
+++ longestSubstring.cpp
@@ -0,0 +1,62 @@
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+//??
+int lengthOfLongestSubstring(string s)
+{
+    std::unordered_map<char, int> charMap;
+    int result = 0, maxResult = 0;
+    for (int i = 0; i < s.length(); ++i)
+    {
+        unordered_map<char, int>::iterator it = charMap.find(s[i]);
+        if (it == charMap.end())
+        {
+            charMap.insert({{s[i], i}});
+            result += 1;
+        }
+        else
+        {
+            result = (i - it->second) > result ? result + 1 : i - it->second;
+        }
+        if (result > maxResult)
+        {
+            maxResult = result;
+        }
+        it->second = i;
+    }
+    return maxResult;
+}
+
+// using vector
+int lengthOfLongestSubstring1(string s)
+{
+    vector<int> table(128, -1);
+    int result = 0, maxResult = 0;
+    for (int i = 0; i < s.length(); ++i)
+    {
+        if (table[s[i]] == -1)
+        {
+            result += 1;
+        }
+        else
+        {
+            result = (i - table[s[i]]) > result ? result + 1 : i - table[s[i]];
+        }
+        if (result > maxResult)
+        {
+            maxResult = result;
+        }
+        table[s[i]] = i;
+    }
+    return maxResult;
+}
+
+int main()
+{
+    lengthOfLongestSubstring("abbcdea");
+    return 0;
+}
